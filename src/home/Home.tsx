e564c9b66fa83967f3ac02e7073325ec9369aeea
Vedran Jerkovic <jerkovicvedran999@gmail.com>
2025-10-25 17:57:22 +0200

    changes

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,6 +1,8 @@
 import "./Home.css";
+import AnimatedContent from '../components/buttons/button-animation.tsx';
 import Silk from "../components/ui/silk-background.tsx";
 import TextType from "../components/ui/text-type.tsx";
+
 
 function Home() {
   return (
@@ -8,18 +10,18 @@ function Home() {
       id="home"
       className="relative min-h-screen overflow-hidden flex items-center justify-center"
     >
-      {/* Background */}
+      {}
       <div className="silk-bg-wrapper">
         <Silk
           speed={5}
-          scale={0.8}       // try adjusting this value for more coverage
+          scale={0.8}       
           color="#4e4eb9"
           noiseIntensity={1.0}
           rotation={0}
         />
       </div>
 
-      {/* Foreground content */}
+      {}
       <div className="relative z-10 text-type">
         <TextType
           text={["Hello, I'm Vedran.", "Welcome To My Portfolio!"]}
@@ -29,8 +31,37 @@ function Home() {
           cursorCharacter="|"
         />
       </div>
+      <AnimatedContent
+        distance={150}
+        direction="vertical"
+        reverse={false}
+        duration={1.2}
+        ease="power3.out"
+        initialOpacity={0.2}
+        animateOpacity
+        scale={1.1}
+        threshold={0.2}
+        delay={0.3}
+>
+  <div>
+    
+   <button
+  className="btn-glow"
+  onClick={() => {
+    const aboutSection = document.getElementById("about");
+    if (aboutSection) {
+      aboutSection.scrollIntoView({ behavior: "smooth" });
+    }
+  }}
+>
+  About Me
+</button>
+  </div>
+</AnimatedContent>
     </section>
   );
 }
 
 export default Home;
+
+
