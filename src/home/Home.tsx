b15f0ad54ca2698d9315891d18861cdd06932bab
Vedran Jerkovic <jerkovicvedran999@gmail.com>
2025-10-25 15:34:21 +0200

    CSS changes and Components added

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,30 +1,36 @@
-import './Home.css';
+import "./Home.css";
+import Silk from "../components/ui/silk-background.tsx";
+import TextType from "../components/ui/text-type.tsx";
 
-import TextType from '../components/ui/text-type.tsx';
-function Home(){
+function Home() {
   return (
-  <section id="home">
-    <div className="text-type">
-      <TextType 
-          text={["Hello, Im Vedran.", "Welcome To My Port"]}
-            typingSpeed={75}
-              pauseDuration={1500}
-            showCursor={true}
+    <section
+      id="home"
+      className="relative min-h-screen overflow-hidden flex items-center justify-center"
+    >
+      {/* Background */}
+      <div className="silk-bg-wrapper">
+        <Silk
+          speed={5}
+          scale={0.8}       // try adjusting this value for more coverage
+          color="#4e4eb9"
+          noiseIntensity={1.5}
+          rotation={0}
+        />
+      </div>
+
+      {/* Foreground content */}
+      <div className="relative z-10 text-type">
+        <TextType
+          text={["Hello, I'm Vedran.", "Welcome To My Portfolio!"]}
+          typingSpeed={150}
+          pauseDuration={1500}
+          showCursor={true}
           cursorCharacter="|"
         />
-    </div>
-  </section>
+      </div>
+    </section>
   );
 }
 
 export default Home;
-
-
-
-
-
-
-
-
-
-
