8be618ee48a4ab6305f1092451dba88cdb2fbabd
Vedran Jerkovic <jerkovicvedran999@gmail.com>
2025-10-28 09:52:30 +0100

    DELETE BACKGROUND

--- src/about/About.tsx
+++ src/about/About.tsx
@@ -1,5 +1,5 @@
 import './About.css';
-import LightRays from '../components/ui/light-ray.tsx';
+
 
 
 function About() {
@@ -9,20 +9,6 @@ function About() {
         <h2>About Me</h2>
         <p>I'm a passionate web developer with a love for coding and design.</p>
    
-        </div>
-        <div className='LightRays'>
-          <LightRays
-            raysOrigin="top-center"
-            raysColor="#00ffff"
-            raysSpeed={1.5}
-            lightSpread={0.8}
-            rayLength={1.2}
-            followMouse={true}
-            mouseInfluence={0.1}
-            noiseAmount={0.1}
-            distortion={0.05}
-            className="custom-rays"
-          />
         </div>
     </section>
   );
